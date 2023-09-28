f065b465182fab385a56c93eb29d41edca4a4645
Inna <prizrak1609@gmail.com>
2023-09-28 17:55:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Створіть клас «Магазин». Необхідно зберігати у полях класу:
+//назву магазину, адреса, опис профілю магазину, контактний телефон, email.
+//Реалізуйте методи класу для введення даних, виведення даних.
+//Реалізуйте доступ до окремих полів через методи класу.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Homework_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Shop shop = new Shop("Мій магазин", "вул. Бажана,10", "Це мій магазин", "1122334455", "my_shop@gmail.com");
+
+            Console.WriteLine("Дані мого магазину:");
+            shop.OutputDataShop();
+            Console.WriteLine();
+
+            shop.InputDataShop("Новий магазин", "вул. Соборна,33", "Це новий магазин", "9988776655", "new_shop@gmail.com");
+            Console.WriteLine("Дані нового магазину:");
+            shop.OutputDataShop();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,46 @@
+﻿//Створіть клас «Магазин». Необхідно зберігати у полях класу:
+//назву магазину, адреса, опис профілю магазину, контактний телефон, email.
+//Реалізуйте методи класу для введення даних, виведення даних.
+//Реалізуйте доступ до окремих полів через методи класу.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Homework_3_3
+{
+    internal class Shop
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public Shop(string name, string address, string description, string phone, string email)
+        {
+            Name = name;
+            Address = address;
+            Description = description;
+            Phone = phone;
+            Email = email;
+        }
+        public void InputDataShop(string name, string address, string description, string phone, string email)
+        {
+            Name = name;
+            Address = address;
+            Description = description;
+            Phone = phone;
+            Email = email;
+        }
+        public void OutputDataShop()
+        {
+            Console.WriteLine($"Назва магазину: {Name}");
+            Console.WriteLine($"Адреса магазину: {Address}");
+            Console.WriteLine($"Опис профілю магазину: {Description}");
+            Console.WriteLine($"Контактний телефон: {Phone}");
+            Console.WriteLine($"Email: {Email}");
+        }
+    }
+}
