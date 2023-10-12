c6981e959509f1b5a832d3b0be681a12e3e7502c
Inna <prizrak1609@gmail.com>
2023-10-12 19:02:26 +0300

    добавила оператор+, оператор- та оператори порівняння

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@
 //Реалізуйте методи класу для введення даних, виведення даних.
 //Реалізуйте доступ до окремих полів через методи класу.
 
+// Додайте до вже створеного класу «Магазин» інформацію про площу магазину.
+// Виконайте навантаження + (для збільшення площі магазину на вказаний розмір),
+// — (для зменшення площі магазину на вказаний розмір),
+// == (перевірка на рівність площ магазинів),
+// < і > (перевірка магазинів менших або більших за площею),
+// != і Equals.Використовуйте механізм властивостей полів класу.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,15 +24,32 @@ namespace NET_Homework_3_3
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            Shop shop = new Shop("Мій магазин", "вул. Бажана,10", "Це мій магазин", "1122334455", "my_shop@gmail.com");
+            Shop shop1 = new Shop("Мій магазин", "вул. Бажана,10", "Це мій магазин", "1122334455", "my_shop@gmail.com", 32000);
 
             Console.WriteLine("Дані мого магазину:");
-            shop.OutputDataShop();
+            shop1.OutputDataShop();
             Console.WriteLine();
 
-            shop.InputDataShop("Новий магазин", "вул. Соборна,33", "Це новий магазин", "9988776655", "new_shop@gmail.com");
+            Shop shop2 = new Shop();
+            shop2.InputDataShop("Новий магазин", "вул. Соборна,33", "Це новий магазин", "9988776655", "new_shop@gmail.com", 56000);
             Console.WriteLine("Дані нового магазину:");
-            shop.OutputDataShop();
+            shop2.OutputDataShop();
+
+            shop1.Comparison(shop1, shop2 );
+
+            shop1.Area += 920;
+            Console.WriteLine($"\nПлоща магазину \"{shop1.Name}\" збільшена на 920 м^2 : {shop1.Area}");
+            shop1.Comparison(shop1 , shop2 );
+
+            shop2.Area -= 32000;
+            Console.WriteLine($"\nПлоща магазину \"{shop2.Name}\" зменшена на 32000 :{shop2.Area}");
+            shop2.Comparison(shop1, shop2);
+
+            Console.WriteLine($"\nПорівняння магазинів Equals (shop1, shop2): {Equals(shop1, shop2)}");
+
+            shop1 = shop2;
+            Console.WriteLine($"Порівняння магазинів Equals (shop1, shop2): {Equals(shop1, shop2)}");
+
             Console.ReadLine();
 
         }
--- Shop.cs
+++ Shop.cs
@@ -3,11 +3,22 @@
 //Реалізуйте методи класу для введення даних, виведення даних.
 //Реалізуйте доступ до окремих полів через методи класу.
 
+// Додайте до вже створеного класу «Магазин» інформацію про площу магазину.
+// Виконайте навантаження + (для збільшення площі магазину на вказаний розмір),
+// — (для зменшення площі магазину на вказаний розмір),
+// == (перевірка на рівність площ магазинів),
+// < і > (перевірка магазинів менших або більших за площею),
+// != і Equals.Використовуйте механізм властивостей полів класу.
+
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NET_Homework_3_3
 {
@@ -18,21 +29,34 @@ namespace NET_Homework_3_3
         public string Description { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public Shop(string name, string address, string description, string phone, string email)
+        public int Area { get; set; }
+
+        public Shop()
         {
-            Name = name;
-            Address = address;
-            Description = description;
-            Phone = phone;
-            Email = email;
+            Name = null;
+            Address = null;
+            Description = null;
+            Phone = null;
+            Email = null;
+            Area = 0;
         }
-        public void InputDataShop(string name, string address, string description, string phone, string email)
+        public Shop(string name, string address, string description, string phone, string email, int area)
         {
             Name = name;
             Address = address;
             Description = description;
             Phone = phone;
             Email = email;
+            Area = area;
+        }
+        public void InputDataShop(string name, string address, string description, string phone, string email, int area)
+        {
+            Name = name;
+            Address = address;
+            Description = description;
+            Phone = phone;
+            Email = email;
+            Area = area;    
         }
         public void OutputDataShop()
         {
@@ -41,6 +65,53 @@ namespace NET_Homework_3_3
             Console.WriteLine($"Опис профілю магазину: {Description}");
             Console.WriteLine($"Контактний телефон: {Phone}");
             Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Площа магазину: {Area}");
+        }
+        public void Comparison(Shop shop1, Shop shop2)
+        {
+            if (shop1.Area != shop2.Area)
+            {
+                Console.WriteLine("\nПлоща магазинів не рівна:");
+                if (shop1.Area < shop2.Area)
+                {
+                    Console.WriteLine($"Площа магазину \"{shop2.Name}\"({shop2.Area}) більша ніж площа магазину \"{shop1.Name}\"({shop1.Area})");
+                }
+                else if (shop1.Area > shop2.Area)
+                {
+                    Console.WriteLine($"Площа магазину \"{shop1.Name}\"({shop1.Area}) більша ніж площа магазину \"{shop2.Name}\"({shop2.Area})");
+                }
+            }
+            else if (shop1.Area == shop2.Area)
+            {
+                Console.WriteLine("\nПлоща магазинів рівна:");
+                Console.WriteLine($"Площа магазину \"{shop1.Name}\"({shop1.Area}) рівна площі магазину \"{shop2.Name}\"({shop2.Area})");
+            }
+        }
+        public static Shop operator+(Shop shop, int n)
+        {
+            shop.Area += n;
+            return shop;
+        }
+        public static Shop operator -(Shop shop, int n)
+        {
+            shop.Area -= n;
+            return shop;
+        }
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1 == shop2;
+        }
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1 != shop2;
+        }
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1 < shop2;
+        }
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1 > shop2;
         }
     }
 }
